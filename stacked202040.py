261f19b65381b9084e74108910240e46c6aeae5c
Henricus Bouwmeester <hbouwmee@msudenver.edu>
2020-04-17 22:08:40 -0600

    example files for stacked courses

---
+++ stacked202040.py
@@ -0,0 +1,5 @@
+# these are courses that fall under the same course
+stackedCourses = []
+
+# these are courses that are at the same dat, time, and location
+stackedCRNs = [[41227, 41233], [41232, 41235]]
